641c27a705519d0f92575b21848cf4b86e3ba1eb
kento <nerationj@gmail.com>
2023-07-01 20:58:31 +0900

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
                     padding: "4px 8px",
                     }}
                 >
-                    きょうのにゃんこ🐈
+                    きょうのにゃんこ
             </button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl} className={styles.img}/>}
